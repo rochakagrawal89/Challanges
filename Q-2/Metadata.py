fcfd923fd235858a65821c638752683ded3b3743
Rochak Agrawal <36627691+rochakagrawal89@users.noreply.github.com>
2022-07-20 20:19:54 +0530

    Update Metadata.py

--- Q-2/Metadata.py
+++ Q-2/Metadata.py
@@ -1,36 +1,44 @@
-#!/usr/bin/env python
 import requests
 import json
-def load():
-    awsurl = '<http://ipaddress/latest'
 
-    awsmetadata = {'dynamic': {}, 'meta-data': {}, 'user-data': {}}
-    for subsect in awsmetadata.keys():
-        dataparse('{0}/{1}/'.format(awsurl, subsect), awsmetadata[subsect])
-    return awsmetadata
+metadata_url = 'http://169.254.169.254/latest/'
 
-def dataparse(url, d):
-    r = requests.get(url)
-    if r.status_code == 404:
-        return
-    for l in r.text.split('\n'):
-        if not l: # "instance-identity/\n" case
-            continue
-        newurl = '{0}{1}'.format(url, l)
-        # a key is detected with a final '/'
-        if l.endswith('/'):
-            newkey = l.split('/')[-2]
-            d[newkey] = {}
-            dataparse(newurl, d[newkey])
+
+def expand_tree(url, arr):
+    output = {}
+    for item in arr:
+        new_url = url + item
+        r = requests.get(new_url)
+        text = r.text
+        if item[-1] == "/":
+            list_of_values = r.text.splitlines()
+            output[item[:-1]] = expand_tree(new_url, list_of_values)
+        elif is_json(text):
+            output[item] = json.loads(text)
         else:
-            r = requests.get(newurl)
-            if r.status_code != 404:
-                try:
-                    d[l] = json.loads(r.text)
-                except ValueError:
-                    d[l] = r.text
-            else:
-                d[l] = None
+            output[item] = text
+    return output
+
+
+def get_metadata():
+    initial = ["meta-data/"]
+    result = expand_tree(metadata_url, initial)
+    return result
+
+
+def get_metadata_json():
+    metadata = get_metadata()
+    metadata_json = json.dumps(metadata, indent=4, sort_keys=True)
+    return metadata_json
+
+
+def is_json(myjson):
+    try:
+        json.loads(myjson)
+    except ValueError:
+        return False
+    return True
+
 
 if __name__ == '__main__':
-    print(json.dumps(load()))
+    print(get_metadata_json())
